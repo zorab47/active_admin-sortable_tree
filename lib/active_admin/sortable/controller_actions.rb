ede079a6a95c3c2922bf801e7207b27f880b5c41
nebirhos <me@nebirhos.com>
2013-06-12 12:02:02 +0200

    Whitespace fix

--- lib/active_admin/sortable/controller_actions.rb
+++ lib/active_admin/sortable/controller_actions.rb
@@ -5,12 +5,12 @@ module ActiveAdmin::Sortable
 
     def sortable(options = {})
       options.reverse_merge! :sorting_attribute => :position,
-	:parent_method => :parent,
-	:children_method => :children,
-	:roots_method => :roots,
-	:tree => false,
-	:max_levels => 0,
-	:protect_root => false
+                             :parent_method => :parent,
+                             :children_method => :children,
+                             :roots_method => :roots,
+                             :tree => false,
+                             :max_levels => 0,
+                             :protect_root => false
 
       # BAD BAD BAD FIXME: don't pollute original class
       @sortable_options = options
@@ -19,27 +19,27 @@ module ActiveAdmin::Sortable
       config.paginate = false
 
       collection_action :sort, :method => :post do
-	resource_name = active_admin_config.resource_name.underscore.parameterize('_')
+        resource_name = active_admin_config.resource_name.underscore.parameterize('_')
 
-	records = params[resource_name].inject({}) do |res, (resource, parent_resource)|
-	  res[resource_class.find(resource)] = resource_class.find(parent_resource) rescue nil
-	res
-	end
-	errors = []
-	ActiveRecord::Base.transaction do
-	  records.each_with_index do |(record, parent_record), position|
-	  record.send "#{options[:sorting_attribute]}=", position
-	  if options[:tree]
-	    record.send "#{options[:parent_method]}=", parent_record
-	  end
-	  errors << {record.id => record.errors} if !record.save
-	  end
-	end
-	if errors.empty?
-	  head 200
-	else
-	  render json: errors, status: 422
-	end
+        records = params[resource_name].inject({}) do |res, (resource, parent_resource)|
+          res[resource_class.find(resource)] = resource_class.find(parent_resource) rescue nil
+          res
+        end
+        errors = []
+        ActiveRecord::Base.transaction do
+          records.each_with_index do |(record, parent_record), position|
+            record.send "#{options[:sorting_attribute]}=", position
+            if options[:tree]
+              record.send "#{options[:parent_method]}=", parent_record
+            end
+            errors << {record.id => record.errors} if !record.save
+          end
+        end
+        if errors.empty?
+          head 200
+        else
+          render json: errors, status: 422
+        end
       end
 
     end
