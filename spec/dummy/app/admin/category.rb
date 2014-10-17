d057ba436dd29469aa450228fb9c9c28c957a0b3
Charles Maresh <charles.maresh@orm-tech.com>
2014-10-16 22:28:49 -0500

    Add Rails 4.1 to the Travis build matrix

--- spec/dummy/app/admin/category.rb
+++ spec/dummy/app/admin/category.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Category do
   sortable
 
-  permit_params :name, :ancestry, :description, :position if ENV['RAILS_VERSION'] == '4.0'
+  permit_params :name, :ancestry, :description, :position if Float(ENV['RAILS_VERSION']) >= 4.0
 
   index as: :sortable do
     label :name
